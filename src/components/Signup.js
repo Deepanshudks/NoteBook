8cb155f36cb3cf93986da0729b09bd48b2cbe894
Deepanshudks <140884017+Deepanshudks@users.noreply.github.com>
2024-07-07 18:27:02 +0530

    Update Signup.js

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -12,7 +12,7 @@ const Signup = (props) => {
     const handleSumit = async (e) => {
       const {name,email,password} = credentials;
         e.preventDefault();
-        const response = await fetch(`http://localhost:5000/api/auth/createuser`, {
+        const response = await fetch(`https://yourbook-c17h.onrender.com/api/auth/createuser`, {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
