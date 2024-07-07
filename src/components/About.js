e0cfa463ef04b0a3b1df43a2656e70b40de18a4a
Deepanshudks <140884017+Deepanshudks@users.noreply.github.com>
2024-07-07 18:45:23 +0530

    Update About.js

--- src/components/About.js
+++ src/components/About.js
@@ -5,7 +5,7 @@ const About = () => {
   return (
     <>
     <div className='conatiner flex text-align justify-center'>
-    <h5>iNoteBook is a platfrom where you can store your notes online at the cloud from anywhere/anytime and get remote access to it.</h5>
+    <h5>YourBook is a platfrom where you can store your notes online at the cloud from anywhere/anytime and get remote access to it.</h5>
     <span className='flex'>&#169; Deepanshudks</span>
     </div>
     </>
